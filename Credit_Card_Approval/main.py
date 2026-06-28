a5b33cb7f0a3da116e5522e7abf026688db090eb
rihab225 <raibak151@gmail.com>
2026-06-29 00:18:28 +0530

    Epic2

--- Credit_Card_Approval/main.py
+++ Credit_Card_Approval/main.py
@@ -1,19 +1,12 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-
-# Read dataset
 app = pd.read_csv("application_record.csv")
-print("Number of people working status:")
-
-print(app["OCCUPATION_TYPE"].value_counts())
-plt.figure(figsize=(18,6))
-
-sns.countplot(
-    x="OCCUPATION_TYPE",
-    data=app,
-    palette="Set2"
+corr = app.corr(numeric_only=True)
+fig, ax = plt.subplots(figsize=(8,6))
+sns.heatmap(
+    corr,
+    annot=True
 )
-plt.xticks(rotation=90)
-plt.title("Occupation Type Distribution")
+plt.title("Correlation Heatmap")
 plt.show()
